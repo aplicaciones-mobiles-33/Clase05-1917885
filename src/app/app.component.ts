91533c8ea3b346072f7d2065e94413e32dee489c
America Gonzaga <america.gonzagatn@uanl.edu.mx>
2021-09-09 13:33:12 -0500

    Practica 05

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'mi-primera-ng-app';
+  title = 'hola, mundo!';
+
+  avatar = '../assets/imagenes/avatar.jpg';
 }
